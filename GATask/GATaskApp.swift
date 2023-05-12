4d27dfa32a0857651e724c2242a21cb1b2688f5d
Amir Mohammadi <amirkamangar@icloud.com>
2023-05-12 12:44:13 +0330

    create an empty project

---
+++ GATask/GATaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  GATaskApp.swift
+//  GATask
+//
+//  Created by Amir Mohammadi on 2/22/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct GATaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
