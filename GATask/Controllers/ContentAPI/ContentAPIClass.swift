79d709f7872863c74beb6283b6e11e066f9cf50e
Amir Mohammadi <amirkamangar@icloud.com>
2023-05-15 17:35:11 +0330

    login function

--- GATask/Controllers/ContentAPI/ContentAPIClass.swift
+++ GATask/Controllers/ContentAPI/ContentAPIClass.swift
@@ -6,7 +6,43 @@
 //
 
 import Foundation
+import Alamofire
+import Semaphore
 
 class ContentAPI {
     public static let shared = ContentAPI()
+    private let baseAPI = "http://138.201.58.73:3030/api/"
+
+    func getAuthToken(username: String, password: String) async -> String? {
+        let semaphore = AsyncSemaphore(value: 0)
+        var requestResponse: String?
+
+        guard let url = URL(string: baseAPI + "login") else { return nil }
+        debugPrint("ContentAPI: Trying to fetch data from URL: \(url)")
+
+        let params: [String: String] = ["username": username,
+                                        "password": password]
+        let headers: HTTPHeaders = ["Content-Type": "application/json"]
+
+        AF.request(url,
+                   method: .post,
+                   parameters: params,
+                   encoder: .json,
+                   headers: headers)
+        .responseDecodable(of: [String: String].self) {
+            (response: DataResponse<[String: String], AFError>) in
+            switch response.result {
+            case .success(let data):
+                requestResponse = data["token"]
+                debugPrint("ContentAPI: Data of type [String: String] fetched.")
+            case .failure(let error):
+                requestResponse = nil
+                debugPrint("ContentAPI: Failed to fetch data of type [String: String]. Alamofire error log: \(error)")
+            }
+            semaphore.signal()
+        }
+        
+        await semaphore.wait()
+        return requestResponse
+    }
 }
